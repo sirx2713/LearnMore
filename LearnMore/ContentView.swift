33a03145807cd8243eff276e9c9f387a36449d3c
iSAD <bradx2713@gmail.com>
2024-07-14 20:44:56 +0200

    Initial Commit

---
+++ LearnMore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnMore
+//
+//  Created by Tafadzwa Alexander on 2024/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnMore/LearnMoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMoreApp.swift
+//  LearnMore
+//
+//  Created by Tafadzwa Alexander on 2024/07/14.
+//
+
+import SwiftUI
+
+@main
+struct LearnMoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
